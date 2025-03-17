d2e14ffb75724ad8c8162b547d48eaa1916addd1
lasha <lasha.dodashvili@gmail.com>
2025-03-18 01:36:55 +0400

    fix build issue

--- src/app/avolta/page.tsx
+++ src/app/avolta/page.tsx
@@ -11,7 +11,7 @@ import { useEmailModalStore } from "@/store/useEmailModal";
 import { useFavoriteGlassesStore } from "@/store/useFavoriteGlassesStore";
 import { useSelectedGlassesStore } from "@/store/useSelectedGlasses";
 import { useTakeSelfieStore } from "@/store/useTakeSelfie";
-// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useState } from "react";
 import { CiHeart } from "react-icons/ci";
@@ -21,12 +21,12 @@ import mainImage from "../../../public/image 2.png";
 import Button from "../../components/button/button";
 import DetailModal from "../../components/modals/detail-modal/detail";
 import Slider from "../../components/slider/slider";
-// const DeepARCanvas = dynamic(
-//   () => import("@/components/deepar-canvas/deepar-canvas"),
-//   {
-//     ssr: false,
-//   }
-// );
+const DeepARCanvas = dynamic(
+  () => import("@/components/deepar-canvas/deepar-canvas"),
+  {
+    ssr: false,
+  }
+);
 
 const Avolta = () => {
   const { openDetailModal } = useDetailModalStore();
@@ -38,7 +38,7 @@ const Avolta = () => {
     (item) => item.objectID === selectedGlasses?.objectID
   );
   const { selfie } = useTakeSelfieStore();
-  // const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <div className="bg-gradient-avolta   pt-8  min-h-screen">
       <div className="px-9">
@@ -48,13 +48,13 @@ const Avolta = () => {
         <div className="relative w-full   h-[80vh] shadow-lg rounded-56px overflow-hidden">
           {activeTab === "Live" ? (
             <div className="flex items-center justify-center h-full relative">
-              {/* {activeTab === "Live" ? (
+              {activeTab === "Live" ? (
                 <DeepARCanvas
                   activeTab={activeTab}
                   setIsLoading={setIsLoading}
                   isLoading={isLoading}
                 />
-              ) : (   )} */}
+              ) : (
                 <Image
                   src={mainImage}
                   alt="Main image"
@@ -62,7 +62,7 @@ const Avolta = () => {
                   style={{ objectFit: "cover", width: "100%", height: "100%" }}
                   className="rounded-56px"
                 />
-           
+              )}
             </div>
           ) : (
             <Image
@@ -80,7 +80,7 @@ const Avolta = () => {
           <TabSelector
             activeTab={activeTab}
             setActiveTab={setActiveTab}
-            // isLoading={isLoading}
+            isLoading={isLoading}
           />
           <div className="absolute bottom-6 right-6 flex gap-14 ">
             <Button
@@ -122,7 +122,7 @@ const Avolta = () => {
       </div>
 
       <Slider />
-      <Footer  />
+      <Footer isLoading={isLoading} />
       <DetailModal />
       <EmailModal />
       <ReceiveSelfie />
