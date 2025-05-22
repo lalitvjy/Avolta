156f774bc281c663f4c70c1547a5133f98df6bf9
Lalit <lalit@styledotme.com>
2025-05-22 17:46:31 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="apple-touch-icon" sizes="180x180" href="https://www.avoltaworld.com/themes/wndrs/assets/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="https://www.avoltaworld.com/themes/wndrs/assets/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="https://www.avoltaworld.com/themes/wndrs/assets/favicons/favicon-16x16.png" />
+      <link rel="manifest" href="https://www.avoltaworld.com/themes/wndrs/assets/favicons/site.webmanifest" />
+      <link rel="mask-icon" href="https://www.avoltaworld.com/themes/wndrs/assets/favicons/safari-pinned-tab.svg" color="#8f53f0" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+
       <body className={`${inter.variable} antialiased`}>
         <div className="relative flex flex-col max-h-screen w-full bg-gray-100">
           {children}
